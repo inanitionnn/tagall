19c869367f755bd6d378525396ecf550dff3c5bd
inanitionnn <tarolv3@gmail.com>
2024-10-01 20:43:47 +0300

    Fix: building issue

--- src/app/(with-navbar)/tags/page.tsx
+++ src/app/(with-navbar)/tags/page.tsx
@@ -1,28 +1,32 @@
 import { TagCategoryList } from "~/app/_components/modules";
 import { CreateTagCategoryDrawer } from "~/app/_components/modules/tags/create-tag-category-drawer";
 import { Button, Header, Input, Wrapper } from "~/app/_components/ui";
-import { api } from "~/trpc/server";
+import { api, HydrateClient } from "~/trpc/server";
+
+export const dynamic = "force-dynamic";
 
 export default async function Tags() {
   void api.tagCategory.getAll.prefetch();
   return (
-    <div className="space-y-8">
-      <div className="flex items-end gap-16">
-        <Header vtag="h2" className="leading-tight">
-          Tags
-        </Header>
-        <Wrapper>
-          <div className="flex items-start justify-between">
-            <CreateTagCategoryDrawer />
-            <div className="flex gap-4">
-              <Button variant="outline">Sort</Button>
-              <Input placeholder="Search" className="w-80" />
+    <HydrateClient>
+      <div className="space-y-8">
+        <div className="flex items-end gap-16">
+          <Header vtag="h2" className="leading-tight">
+            Tags
+          </Header>
+          <Wrapper>
+            <div className="flex items-start justify-between">
+              <CreateTagCategoryDrawer />
+              <div className="flex gap-4">
+                <Button variant="outline">Sort</Button>
+                <Input placeholder="Search" className="w-80" />
+              </div>
             </div>
-          </div>
-        </Wrapper>
-      </div>
+          </Wrapper>
+        </div>
 
-      <TagCategoryList />
-    </div>
+        <TagCategoryList />
+      </div>
+    </HydrateClient>
   );
 }
