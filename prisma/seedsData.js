ed18974bd07c711cafb73120d5ed960505061091
inanitionnn <tarolv3@gmail.com>
2024-12-19 21:52:27 +0200

    Refactor: update fieldGroups in seedsData for improved organization and clarity

--- prisma/seedsData.js
+++ prisma/seedsData.js
@@ -23,6 +23,43 @@ export const seedsData = {
   ],
   fieldGroups: [
     {
+      id: "cm4vpygr80004ius76k8f8ra1",
+      name: "genres",
+      priority: 1,
+      isFiltering: true,
+      collections: [
+        "cm3bnlyzw00040ks77yx39ddf",
+        "cm3a9sw1v000gi1s7appi2tju",
+        "cm3bnlve100000ks784yufzt9",
+      ],
+    },
+    {
+      id: "cm3a9sqbe000ci1s74wjod48c",
+      name: "contentRating",
+      priority: 2,
+      isFiltering: true,
+      collections: ["cm3a9sw1v000gi1s7appi2tju", "cm3bnlve100000ks784yufzt9"],
+    },
+    {
+      id: "cm3a9so0i000ai1s77en3aul5",
+      name: "keywords",
+      priority: 3,
+      isFiltering: true,
+      collections: [
+        "cm3bnlyzw00040ks77yx39ddf",
+        "cm3a9sw1v000gi1s7appi2tju",
+        "cm3bnlve100000ks784yufzt9",
+      ],
+    },
+
+    {
+      id: "cm3a9sse6000ei1s7cvwshatu",
+      name: "production",
+      priority: 4,
+      isFiltering: true,
+      collections: ["cm3a9sw1v000gi1s7appi2tju", "cm3bnlve100000ks784yufzt9"],
+    },
+    {
       id: "cm3a9sm6c0008i1s7ekkm28ed",
       name: "people",
       priority: 5,
@@ -34,31 +71,6 @@ export const seedsData = {
       ],
     },
     {
-      id: "cm3a9so0i000ai1s77en3aul5",
-      name: "keywords",
-      priority: 2,
-      isFiltering: true,
-      collections: [
-        "cm3bnlyzw00040ks77yx39ddf",
-        "cm3a9sw1v000gi1s7appi2tju",
-        "cm3bnlve100000ks784yufzt9",
-      ],
-    },
-    {
-      id: "cm3a9sqbe000ci1s74wjod48c",
-      name: "contentRating",
-      priority: 3,
-      isFiltering: true,
-      collections: ["cm3a9sw1v000gi1s7appi2tju", "cm3bnlve100000ks784yufzt9"],
-    },
-    {
-      id: "cm3a9sse6000ei1s7cvwshatu",
-      name: "production",
-      priority: 4,
-      isFiltering: true,
-      collections: ["cm3a9sw1v000gi1s7appi2tju", "cm3bnlve100000ks784yufzt9"],
-    },
-    {
       id: "cm3hvqbhn0000hxs7gckv6nyi",
       name: "runtime",
       priority: 7,
@@ -66,14 +78,14 @@ export const seedsData = {
       collections: ["cm3a9sw1v000gi1s7appi2tju"],
     },
     {
-      id: "cm3hvqbhn0000hxs7gckv6nyi",
+      id: "cm4vpy3cr0000ius71dyb4il2",
       name: "volumes",
       priority: 8,
       isFiltering: false,
       collections: ["cm3bnlyzw00040ks77yx39ddf"],
     },
     {
-      id: "cm3hvqbhn0000hxs7gckv6nyi",
+      id: "cm4vpy5dz0002ius7d2ywbphx",
       name: "chapters",
       priority: 9,
       isFiltering: false,
