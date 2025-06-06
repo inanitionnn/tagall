33e52d100cd78aad24a9648174ff7d59f5e1959b
inanitionnn <tarolv3@gmail.com>
2025-06-06 18:56:36 +0000

    refactor: enhance BackgroundImage component structure for improved responsiveness and layout

--- src/app/_components/shared/background-image.tsx
+++ src/app/_components/shared/background-image.tsx
@@ -6,17 +6,24 @@ type Props = ComponentPropsWithoutRef<"div"> & { image: string };
 export const BackgroundImage = (props: Props) => {
   const { children, image, ...restProps } = props;
   return (
-    <div className="relative h-full min-h-screen w-full">
-      <Image
-        src={image}
-        alt="background"
-        width={1200}
-        height={675}
-        className="fixed inset-0 -z-10 h-[100dvh] w-full object-cover object-center opacity-[0.10]"
-        {...restProps}
-      />
+    <div className="relative h-full min-h-[100dvh] w-full">
+      {/* Fixed positioned background container */}
+      <div className="fixed inset-0 -z-10 h-[100dvh] w-[100dvw] overflow-hidden">
+        <Image
+          src={image}
+          alt="background"
+          width={1200}
+          height={675}
+          className="h-full w-full object-cover object-center opacity-[0.10]"
+          priority
+          {...restProps}
+        />
+      </div>
 
-      {children}
+      {/* Content container with proper positioning */}
+      <div className="relative z-0 h-full min-h-[100dvh] w-full">
+        {children}
+      </div>
     </div>
   );
 };
