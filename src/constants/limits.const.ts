fb02138800dedf92a818bb6117aa059e464c5bb0
Oleksandr Tarasiuk <tarolv3@gmail.com>
2026-02-12 23:35:13 +0200

    refactor: update DEFAULT_ADD_LIMIT constant to improve configuration flexibility

--- src/constants/limits.const.ts
+++ src/constants/limits.const.ts
@@ -1,3 +1,3 @@
 export const DEFAULT_RANDOM_LIMIT = 6 as const;
 export const DEFAULT_HOME_LIMIT = 30 as const;
-export const DEFAULT_ADD_LIMIT = 10 as const;
+export const DEFAULT_ADD_LIMIT = 30 as const;
